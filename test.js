e03c9b1a0ac7297480ab4d6685618e6ff62d33f1
furofo <chriscellphonerepair@gmail.com>
2021-05-14 00:21:20 -0400

    first test of switching session cookies with button

--- test.js
+++ test.js
@@ -1,21 +1,19 @@
-console.log("helloworld");
-console.log("testing cookies");
+
 //localStorage.setItem('firstCookie', 'You set your first cookie');
 //localStorage.setItem('secondCookie', 'your second cookie');
 const cookie = localStorage.getItem('firstCookie');
-console.log("this is your cookie" + cookie);
-
-for (var i = 0; i < localStorage.length; i++){
-    console.log("looping through cookies");
-    console.log(localStorage.getItem(localStorage.key(i)));
-}
-
+let switcher = 'off';
 function changeText() {
     var change = document.getElementById("test");
-    if(change.innerHTML == "test") {
-        change.innerHTML = "changed";
+    if(switcher == 'off') {
+        localStorage.setItem('firstCookie', 'You changed your first cookie');
+        console.log(localStorage.getItem('firstCookie'));
+        switcher = 'on';
     }
     else {
-        change.innerHTML = "test";
+        localStorage.setItem('firstCookie', 'You changed your first cookie back to basics');
+        console.log(localStorage.getItem('firstCookie'));
+        switcher = 'off';
+        
     }
 }
