4c624e9962301bb9df6de8e8bd6b56ddde26b71e
furofo <chriscellphonerepair@gmail.com>
2021-05-14 00:12:16 -0400

    added button wiht function to change text on click

--- test.js
+++ test.js
@@ -1,10 +1,21 @@
 console.log("helloworld");
 console.log("testing cookies");
 //localStorage.setItem('firstCookie', 'You set your first cookie');
+//localStorage.setItem('secondCookie', 'your second cookie');
 const cookie = localStorage.getItem('firstCookie');
 console.log("this is your cookie" + cookie);
 
 for (var i = 0; i < localStorage.length; i++){
     console.log("looping through cookies");
     console.log(localStorage.getItem(localStorage.key(i)));
+}
+
+function changeText() {
+    var change = document.getElementById("test");
+    if(change.innerHTML == "test") {
+        change.innerHTML = "changed";
+    }
+    else {
+        change.innerHTML = "test";
+    }
 }
